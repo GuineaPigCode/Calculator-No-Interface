75806f27a3bfe7f9f8dd576bd97f134aa1d5f662
GuineaPigCode <93527295+GuineaPigCode@users.noreply.github.com>
2022-01-20 13:25:03 -0300

    Update and rename Caculadora.py to start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+print('----> Starting <----')
+
+n1 = int(input('Add with what number > '))
+n2 = int(input('Add with the main number > '))
+soma = (n1+n2)
+
+print('Sum Between {} and {} Sum To Total: {}'.format(n1, n2, soma))
+
+# Debug
+print('Debug')
+print(type(n1))
+print(type(n2))
+print(type(soma))
