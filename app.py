4416574aa1997681fd5eace7a10c9e4bd4e51ab4
✨GuineaPig✨ <93527295+GuineaPigCode@users.noreply.github.com>
2022-01-21 14:22:40 -0300

    More Formats yay

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-# import os 
+start = ('Calculator No Interface')
 
-print('----> Calculator No Iterface <----')
+print('{:=^30}'.format(start))
 
-v = ('1.1')
-n1 = int(input('Add with what number > '))
-n2 = int(input('Add with the main number > '))
-n12 = (n1+n2)
+text1s = ('Sum')
+text2m = ('Multiplication')
+text3d = ('Division')
 
-print('Sum Between {} and {} Sum To Total: {}'.format(n1, n2, n12))
+text4di = ('Integer Division')
+text5e = ('Power')
 
-# Debug
-# print('Debug')
-# print(type(n1))
-# print(type(n2))
-# print(type(n12))
 
-print('Version: {}'.format(v))
+n1 = int(input('Add with what number\n > '))
+n2 = int(input('Add with the main number\n > '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+
+di = n1 // n2
+e = n1 ** n2
+
+print('\n{:=^20}'.format(text1s))
+print('Sum To The Total Of {}\n'.format(s))
+
+print('{:=^20}'.format(text2m))
+print('Multiplication To Total Of {}\n'.format(m))
+
+print('{:=^20}'.format(text3d))
+print('Division To Total Of {}\n'.format(d))
+
+print('{:=^20}'.format(text4di))
+print('Integer Division To Total Of {}\n'.format(di))
+
+print('{:=^20}'.format(text5e))
+print('Total Power Of {}\n'.format(e))
