f8aec734801909416368cc0656235042434c14e1
✨GuineaPig✨ <93527295+GuineaPigCode@users.noreply.github.com>
2022-01-21 14:24:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-# import os 
+start = ('Calculadora Sem Interface')
 
-print('----> Calculator No Iterface <----')
+print('{:=^30}'.format(start))
 
-v = ('1.1 PT-BR')
-n1 = int(input('Somar Com Qual Número > '))
-n2 = int(input('Somar Com O Número Principal > '))
-n12 = (n1+n2)
+text1s = ('Soma')
+text2m = ('Multiplicação')
+text3d = ('Divição')
 
-print('Soma Entre {} e {} Soma Ao Total: {}'.format(n1, n2, n12))
+text4di = ('Divição Inteira')
+text5e = ('Potência')
 
-# Debug
-# print('Debug')
-# print(type(n1))
-# print(type(n2))
-# print(type(n12))
 
-print('Versão: {}'.format(v))
+n1 = int(input('Somar Com Qual Número\n > '))
+n2 = int(input('Somar Com O Número Principal\n > '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+
+di = n1 // n2
+e = n1 ** n2
+
+print('\n{:=^20}'.format(text1s))
+print('Soma Ao Total De {}\n'.format(s))
+
+print('{:=^20}'.format(text2m))
+print('Multiplicação Ao Total De {}\n'.format(m))
+
+print('{:=^20}'.format(text3d))
+print('Divição Ao Total De {}\n'.format(d))
+
+print('{:=^20}'.format(text4di))
+print('Divição Inteira Ao Total De {}\n'.format(di))
+
+print('{:=^20}'.format(text5e))
+print('Potência Ao Total De {}\n'.format(e))
